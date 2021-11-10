11a70ecad30f27fbd75cb53aeb79deaaef753d17
Desiree Quade <nekodez@gmail.com>
2021-11-10 13:03:02 -0600

    fixed mistakes

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -28,8 +28,6 @@ console.log('4. Removed item:');
 
 let removedS=supplyChanges.pop();
 
-supplyChanges.pop();
-
 console.log(removedS);
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
